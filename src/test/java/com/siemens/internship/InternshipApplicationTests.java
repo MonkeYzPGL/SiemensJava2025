0ce10b9434c13326b20a8cb94c0eb6406eb95443
Elecfi Sergiu <elecfis@gmail.com>
2025-05-09 12:41:17 +0300

    Update InternshipApplicationTests.java

--- src/test/java/com/siemens/internship/InternshipApplicationTests.java
+++ src/test/java/com/siemens/internship/InternshipApplicationTests.java
@@ -1,13 +1,35 @@
 package com.siemens.internship;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class InternshipApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext, "The application context should not be null");
 	}
 
+	@Test
+	void itemControllerLoads() {
+		assertTrue(applicationContext.containsBean("itemController"), "ItemController should be loaded");
+	}
+
+	@Test
+	void itemServiceLoads() {
+		assertTrue(applicationContext.containsBean("itemService"), "ItemService should be loaded");
+	}
+
+	@Test
+	void itemRepositoryLoads() {
+		assertTrue(applicationContext.containsBean("itemRepository"), "ItemRepository should be loaded");
+	}
 }
